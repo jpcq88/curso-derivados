24a698f6de670616dcf7b594910a9b65e2154e48
Juan Pablo <jpcq88@gmail.com>
2015-10-07 21:50:27 -0500

    inicio aller 2.

---
+++ taller2/r/taller2.R
@@ -0,0 +1,11 @@
+## -----------------------------------------------------------------------------
+#
+# Taller 2: Derivados financieros
+# Juan Pablo Calle Quintero
+#
+## -----------------------------------------------------------------------------
+
+## Librerías requeridas
+
+library(fGarch) # estimación de modelos GARCH
+library(ggplot) # gráficar
