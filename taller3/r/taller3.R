e7116b45e6414099d1beee9dde851921754f3210
Juan Pablo <jpcq88@gmail.com>
2015-11-16 22:18:22 -0500

    avance pinto 2a.

--- taller3/r/taller3.R
+++ taller3/r/taller3.R
@@ -6,16 +6,18 @@
 # Última modificación: 16 de noviembre de 2015
 # 
 # Puede descragar este código en el siguiente enlace:
-# https://github.com/jpcq88/curso-derivados/blob/master/taller2/r/taller2.R
+# https://github.com/jpcq88/curso-derivados/tree/master/taller3
 #
 ## -----------------------------------------------------------------------------
 
 ## Librerías requeridas --------------------------------------------------------
 
+library(fOptions) # valoración de opciones
 library(ggplot2) # gráficos de calidad
 library(gridExtra) # herramientas adicionales para graficar, grid.arrange()
 library(data.table) # manipulación de datos
 library(RQuantLib) # funciones varias de finanzas cuantitativas
+library(GUIDE) # otras funciones para valorar opciones
 
 
 ## Definición de funciones -----------------------------------------------------
@@ -59,6 +61,21 @@ sim_st <- function(tpo, S0, mu = 0.15, sigma = 0.25, r = mu, dt = 0.01) {
   return(St)
 }
 
+ret_crit <- function(x, v.crit) {
+  # función para identificar los valores críticos
+  tmp <- vector(length = length(x))
+  for (i in seq_along(tmp)) {
+    if (x[i] < -abs(v.crit)) {
+      tmp[i] <- '< val crit'
+    } else if (x[i] > abs(v.crit)) {
+      tmp[i] <- '> val crit'
+    } else {
+      tmp[i] <- 'x'
+    }
+  }
+  return(tmp)
+}
+
 
 ## Lectura de datos ------------------------------------------------------------
 
@@ -67,6 +84,7 @@ sp500_idx[, fecha := as.Date(fecha, format = '%Y-%m-%d')]
 sp500_idx
 sp500_opt <- fread('../datos/datos_sp500_opt.csv', header = TRUE)
 sp500_opt
+strike <- read.csv('../datos/strike.csv', header = TRUE)
 
 
 ## Análisis descriptivo --------------------------------------------------------
@@ -95,263 +113,104 @@ dev.off()
 
 # a)
 
+# del 4 de noviembre al tercer viernes de diciembre (18/12/2015) hay 44 días
+# El precio de cierre del índice S&P500 el 4 de niviembre era 2102.31
+# El precio de ejercicio es 2110.20
 
+vol_imp <- function(option_value) {
+  iv <- EuropeanOptionImpliedVolatility(type = 'call', value = option_value,
+                                  underlying = 2102.31, strike = 2110.20,
+                                  riskFreeRate = 0.05, dividendYield = 0,
+                                  maturity = Tmat, volatility = 0.25)
+  return(iv[1])
+}
+
+Tmat <- as.numeric(difftime("2015-12-18","2015-11-04")) / 365.24
+EuropeanOptionImpliedVolatility(type = 'call', value = 153.65,
+                                underlying = 2102.31, strike = 2110.20,
+                                riskFreeRate = 0.05, dividendYield = 0,
+                                maturity = Tmat, volatility = 0.25)[1]
+vol_imp(153.24)
+imp_vol <- sapply(sp500_opt[1:43, sp500_opt], vol_imp)
+precio_opt <- sp500_opt[1:43, sp500_opt]
+
+dat_tmp <- data.frame(strike$strike, imp_vol)
+
+pdf('../img/p1a_grafico.pdf', width = 5, height = 5)
+ggplot(data = dat_tmp) + theme_bw(12) +
+  geom_line(aes(x = strike.strike, y = imp_vol)) +
+  labs(x = 'Strike', y = 'Volatilidad implícita')
+dev.off()
+
+# b)
 
 # Calcular los log-retornos del índice S&P500, quedan almacenados en sp500_r
 sp500_idx[, sp500_r := c(NA, # se pierde el primer dato al diferenciar
                         log(sp500[2:.N]) - log(sp500[1:(.N - 1)]))]
 sp500_idx
 
-# Para hacer el código reproducible fijemos la semilla del generador de
-# números aleatorios.
-set.seed(123)
+# Cálculo volatilidad diaria de los log-retornos del índice S&P500
+(mu_ret_sp500_dia <- sp500_idx[, mean(sp500_r, na.rm = TRUE)])
+(sd_ret_sp500_dia <- sp500_idx[, sd(sp500_r, na.rm = TRUE)])
+sd_ret_sp500_dia * sqrt(252)
+val_critico <- 3 * sd_ret_sp500_dia
 
-# Definamos parámetros del modelo
-tpo_al_venc <- 1 # en años
-K <- 2000 # precio acordado al vencimiento
-sigma_op <- 0.1 # volatilidad de la opción (anualizada)
-r_op <- 0.05 # tasa libre de riesgo para valoración de la oción (anualizada)
-S0 <- 1930 # precio a vista del subyacente
-trading_days <- 252 # número de días de negociación en un año en EEUU
-year_days <- 365 # número de días en un año
+ret_2a <- sp500_idx[(.N - 2 * 252):.N, .(fecha, sp500_r)]
 
-# Los siguientes parámetros son los calculados con base en los datos históricos
-mu_day <- datos_p1[, mean(sp500_r, na.rm = TRUE)] # retorno promedio diario
-mu_ann <- exp(mu_day * trading_days) - 1 # retorno anualizado
-sigma_day <- datos_p1[, sd(sp500_r, na.rm = TRUE)] # volatilidad diaria promedio
-sigma_ann <- sigma_day * sqrt(trading_days) # volatilidad anualizada
+ret_2a[, val_crit := ret_crit(sp500_r, val_critico)]
 
-# simular una realización del precio de la acción
-simul_st <- sim_st(tpo_al_venc, S0 = S0,
-                   mu = mu_ann, sigma = sigma_op, # usando sigma de la opción
-                   r = r_op, dt = tpo_al_venc / year_days)
+ret_2a[, table(val_crit)] # frecuencia
+ret_2a[, prop.table(table(val_crit))] # proporciones
 
-# Graficar una realización del precio de la acción
-# Guarda el gráfico en la carpeta img con el nombre p1_simul_precio.pdf
-pdf(file = '../img/p1_simul_precio.pdf', width = 5, height = 4)
+ret_2a[, col_val_crit := 'black'] # definir colores del gráfico
+ret_2a[val_crit == '< val crit', col_val_crit := 'red']
+ret_2a[val_crit == '> val crit', col_val_crit := 'blue']
 
-opar <- par(no.readonly = TRUE)
-par(mar = c(2, 4, 1, 1))
-plot(simul_st, type = 'l',
-     xlab = '',
-     ylab = 'Precio de la acción ',
-     xlim = c(0, year_days + 40), cex.lab = 0.8, cex.axis = 0.6)
-abline(h = K, lty = 2, col = 'red')
-abline(h = S0, lty = 2, col = 'red')
-text(labels = paste('ST =', round(simul_st[year_days])),
-     cex = 0.6, col = 'red', pos = 3,
-     x = year_days + 20, y = simul_st[year_days])
-text(labels = paste('K =', round(K, 2)),
-     cex = 0.6, col = 'red', pos = 3,
-     x = year_days + 20, y = K)
-text(labels = paste('S0 =', round(S0, 2)),
-     cex = 0.6, col = 'red', pos = 3,
-     x = year_days + 20, y = S0)
-par(opar)
-
+# Guardar gráfico en pdf
+pdf('../img/p1b_grafico.pdf', width = 5, height = 5)
+ggplot(data = ret_2a) + theme_bw(12) +
+  geom_line(aes(x = fecha, y = sp500_r)) +
+  geom_point(aes(x = fecha, y = sp500_r), colour = ret_2a[, col_val_crit]) +
+  geom_hline(yintercept = c(-val_critico, val_critico), linetype = 2) +
+  labs(y = 'log-retornos índice S&P500')
 dev.off()
-
-nro_replicas <- 1000 # número de réplicas (muestras) simulación munte carlo
-
-# Simalar 1000 posibles realizaciones del precio de la acción
-simul_precios <- replicate(nro_replicas,
-                           sim_st(tpo_al_venc, S0 = S0,
-                                 mu = mu_ann, sigma = sigma_op,
-                                 r = r_op, dt = tpo_al_venc / year_days))
-
-# Graficar 1000 realizaciones del precio de la acción
-# Guarda el gráfico en la carpeta img con el nombre p1_simul_precios.pdf
-pdf(file = '../img/p1_simul_precios.pdf', width = 5, height = 4)
-
-opar <- par(no.readonly = TRUE)
-par(mar = c(2, 4, 1, 1), cex.lab = 0.8, cex.axis = 0.6)
-ts.plot(simul_precios, lwd = 0.2,
-        xlab = '', ylab = 'Precio de la acción', xlim = c(0, year_days + 40))
-abline(h = K, col = 'red', lty = 2)
-abline(h = S0, lty = 2, col = 'red')
-text(labels = paste('K =', round(K, 2)),
-     cex = 0.6, col = 'red', pos = 3,
-     x = year_days + 30, y = K)
-text(labels = paste('S0 =', round(S0, 2)),
-     cex = 0.6, col = 'red', pos = 1,
-     x = year_days + 30, y = S0)
-par(opar)
-
-dev.off()
-
-# Valor esperado de $max((S_T - K)^+, 0)$
-val_esp_St_K <- mean(pmax(simul_precios[year_days, ] - K, 0))
-
-# Valor de la opción obtenido con la simulación
-(C_St <- exp(-r_op * 1) * val_esp_St_K)
-
-# b)
-
-# Valor de la opción usando la fórmula de Black-Scholes
-GBSOption(TypeFlag = 'c', S = 1930, X = 2000, Time = 1, r = 0.05, b = 0.05,
-          sigma = 0.1)@price
-
-# Valor de la opción con Black-Scholes calculado manualmaente
-sigma_op_day <- sigma_op / sqrt(trading_days)
-r_op_day <- exp(-r_op * trading_days)
-tpo_al_venc_day <- tpo_al_venc * 365
-d1 <- ((r_op + 0.5 * (sigma_op ^ 2)) * tpo_al_venc + log(S0 / K)) /
-  (sigma_op * sqrt(tpo_al_venc))
-d2 <- ((r_op - 0.5 * (sigma_op ^ 2)) * tpo_al_venc + log(S0 / K)) /
-  (sigma_op * sqrt(tpo_al_venc))
-(C_bs <- pnorm(d1) * S0 - pnorm(d2) * K * exp(-r_op * tpo_al_venc))
 
 # c)
 
-# gráfico log-retornos índice S&P500
-ggplot(data = datos_p1, aes(x = Date, y = sp500_r)) + theme_bw(12) +
-  geom_line() +
-  geom_hline(yintercept = datos_p1[, mean(sp500_r, na.rm = TRUE)],
-             colour = 'red', linetype = 2) +
-  labs(x = '',
-       y = 'log retornos S&P500')
-
-datos_p1[.N] # la última fecha es el 2 de octubre de 2015
-
-# Ajuste modelo garch(1,1) para los log-retornos del índice S&P500
-garch11 <- garchFit(formula = ~garch(1, 1), data = datos_p1[2:.N, sp500_r])
-
-garch11@fit # parámetros estimados
-garch11@sigma.t # varianzas estimadas
-
-# Gráfico de volatilidad estimada
-pdf(file = '../img/p1c_sigma_est.pdf', width = 2.5, height = 1.5)
-
-opar <- par(no.readonly = TRUE)
-par(mar = c(1, 2, 1, 1), cex.lab = 0.4, cex.axis = 0.4)
-plot(sqrt(garch11@sigma.t),
-     type = 'l', xlab = '', ylab = '', bty = 'n', xaxt = 'n',
-     ylim = c(0.06, 0.16), tcl = -0.2, lwd = 0.6)
-par(opar)
-
-dev.off()
-
-# Extraer los valores de los parámetros estimados
-omega <- garch11@fit$matcoef[1, 1]
-alpha1 <- garch11@fit$matcoef[2, 1]
-beta1 <- garch11@fit$matcoef[3, 1]
-
-# Estimación $\sigma^2$ para el 3 de octubre (un día adelante)
-sigma2_1 <- omega + alpha1 * garch11@residuals[nrow(datos_p1) - 1] ^ 2 +
-  beta1 * garch11@h.t[nrow(datos_p1) - 1]
-sqrt(sigma2_1) # pronóstico volatilidad diaria estimada un día adelante
-
-# Estimación $\sigma^2$ para el 4 de octubre (dos días adelante)
-sigma2_2 <- omega + (alpha1 + beta1) * sigma2_1
-sqrt(sigma2_2) # pronóstico volatilidad diaria estimada dos días adelante
-
-# Estimación $\sigma^2$ para el 5 de octubre (tres días adelante)
-sigma2_3 <- omega + (alpha1 + beta1) * sigma2_2
-sqrt(sigma2_3) # pronóstico volatilidad estimada tres días adelante (05/10/2015)
-sqrt(sigma2_3)*sqrt(trading_days) # anualizada
+curve(dlnorm(x, 0, 0.25), xlim = c(0, 3), ylim = c(0, 2))
+curve(dlnorm(x, 0, 0.5), add = TRUE)
 
 
 ## Punto 2 ---------------------------------------------------------------------
 
-# Definición de parámetros del portafolio
-acciones_att <- 2000 # número de acciones de AT&T
-acciones_msoft <- 1000 # número de acciones de Microsoft
-
-# Calcular los $\Delta_P$ (port_d) y $\Delta_F$ (sp500_d)
-# Diferencias futuro 3 meses índice S&P500
-datos_p2[, sp500_d := c(NA, # se pierde el primer dato al diferenciar
-                        sp500[2:.N] - sp500[1:(.N - 1)])]
-# Diferencias acción AT&T
-datos_p2[, att_d := c(NA, # se pierde el primer dato al diferenciar
-                      att[2:.N] - att[1:(.N - 1)])]
-# Diferencias acción Microsoft
-datos_p2[, msoft_d := c(NA, # se pierde el primer dato al diferenciar
-                        msoft[2:.N] - msoft[1:(.N - 1)])]
-# Valor del portafolio
-datos_p2[, port := .(msoft[1:.N] * acciones_msoft +
-                       att[1:.N] * acciones_att)]
-# Diferencias valor del portafolio
-datos_p2[, port_d := c(NA, port[2:.N] - port[1:(.N - 1)])]
-datos_p2
-
 # a)
 
-# Ajuste regresión $\Delta S = \beta_0 + \beta_1 \Delta F + \epsilon_t$
-reg <- lm(port_d ~ sp500_d, data = datos_p2)
-summary(reg)
+K1 <- 25 # long call strike
+K2 <- 30 # short call strike
+S0 <- 32 # precio spot del subyacente
+sigma <- 0.3 # volatilidad anual: 30 \%
+tasa_lr <- 0.05 # tasa anual libre de riesgos: 5 \%
+tiempo_venc <- 0.5 # tiempo al vencimiento: 6 meses
 
-# Coeficiente de correlación $\rho$
-rho <- sqrt(summary(reg)$r.squared)
+precio_k1 <- GBSOption(TypeFlag = 'c',
+                       S = S0,
+                       X = K1,
+                       Time = tiempo_venc,
+                       r = tasa_lr,
+                       b = tasa_lr,
+                       sigma = sigma)@price
 
-# Radio óptimo de cobertura $h^*$
-vol_port <- datos_p2[, sd(port, na.rm = TRUE)] # volatilidad portafolio
-vol_sp500 <- datos_p2[, sd(sp500, na.rm = TRUE)] # volatilidad futuro S&P500
-rad_cob <- rho * (vol_port / vol_sp500)
+precio_k2 <- GBSOption(TypeFlag = 'c',
+                       S = S0,
+                       X = K2,
+                       Time = tiempo_venc,
+                       r = tasa_lr,
+                       b = tasa_lr,
+                       sigma = sigma)@price
 
-# b)
+(bull_spred <- precio_k1 - precio_k2)
+bullspreadcalls()
 
-# Número óptimo de contratos $N^*$
-vlr_port <- datos_p2[.N, port]
-vlr_fut <- 250 * datos_p2[.N, sp500]
-nro_cont <- rad_cob * (vlr_port / vlr_fut)
-
-# Gráfico de regresión
-pdf(file = '../img/p2_reg.pdf', width = 5, height = 4)
-
-ggplot(data = datos_p2, aes(x = sp500_d, y = port_d)) + theme_bw(9) +
-  geom_point(size = 1.5) +
-  stat_smooth(method = 'lm', na.rm = TRUE, fill = 'gray', alpha = 0.2) +
-  labs(x = 'Delta S&P500',
-       y = 'Delta portafolio AT&T y Microsoft Corp.') +
-  annotate('text', label = paste('rho ==', round(rho, 4)),
-           x = -100, y = 10000, parse = TRUE, size = 4)
-
-dev.off()
-
-# El mismo gráfico pero más pequeño para poner al margen
-pdf(file = '../img/p2_reg_margen.pdf', width = 2.5, height = 1.5)
-
-ggplot(data = datos_p2, aes(x = sp500_d, y = port_d)) + theme_bw(5) +
-  geom_point(size = 1) +
-  stat_smooth(method = 'lm', na.rm = TRUE, fill = 'gray', alpha = 0.2) +
-  labs(x = 'Delta S&P500',
-       y = 'Delta portafolio') +
-  annotate('text', label = paste('rho ==', round(rho, 4)),
-           x = -100, y = 10000, parse = TRUE, size = 1.5) +
-  theme(axis.ticks = element_blank())
-
-dev.off()
-
-# c)
-
-# Estimaciones de la media y desviación estándar del portafolio $\Delta P$
-mu_port <- datos_p2[, mean(port_d, na.rm = TRUE)]
-sigma_port <- datos_p2[, sd(port_d, na.rm = TRUE)]
-
-# Cálculo valor del portafolio $\Delta X = \Delta P - N^* \Delta F$
-datos_p2[, x_d := .(port_d - nro_cont * sp500_d)]
-
-# Estimaciones de la media y desviación estándar del portafolio $\Delta X$
-mu_xd <- datos_p2[, mean(x_d, na.rm = TRUE)]
-sigma_xd <- datos_p2[, sd(x_d, na.rm = TRUE)]
-
-nobs_simul <- 1000 # número de simulaciones
-
-# Simulamos los portafolios y los guardamos
-dat_simul <- data.table(serie = rep(c('dP', 'dX'), each = nobs_simul),
-                        sim = c(
-                          rnorm(nobs_simul, mean = mu_port, sd = sigma_port),
-                          rnorm(nobs_simul, mean = mu_xd, sd = sigma_xd))
-                        )
-dat_simul
-
-# Gráfico de las densidades de los portafolios $\Delta P$ y $\Delta X$
-pdf(file = '../img/p2_densidad.pdf', width = 6, height = 4)
-
-ggplot(data = dat_simul, aes(colour = serie)) + theme_bw(10) +
-  geom_density(aes(x = sim)) +
-  labs(x = 'Valor del portafolio',
-       y = 'Densidad') +
-  theme(legend.title = element_blank())
-
-dev.off()
+# Para hacer el código reproducible fijemos la semilla del generador de
+# números aleatorios.
+set.seed(123)
