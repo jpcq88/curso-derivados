00b3d935172b639a3b16217ec6cf45f6e18d42c9
Juan Pablo <jpcq88@gmail.com>
2015-11-21 17:29:13 -0500

    taller 3 finalizado.

--- taller3/r/taller3.R
+++ taller3/r/taller3.R
@@ -3,7 +3,7 @@
 # Taller 3: Derivados Financieros
 # Juan Pablo Calle Quintero
 # jpcalleq@unal.edu.co
-# Última modificación: 16 de noviembre de 2015
+# Última modificación: 21 de noviembre de 2015
 # 
 # Puede descragar este código en el siguiente enlace:
 # https://github.com/jpcq88/curso-derivados/tree/master/taller3
@@ -17,7 +17,7 @@ library(ggplot2) # gráficos de calidad
 library(gridExtra) # herramientas adicionales para graficar, grid.arrange()
 library(data.table) # manipulación de datos
 library(RQuantLib) # funciones varias de finanzas cuantitativas
-library(GUIDE) # otras funciones para valorar opciones
+library(moments) # calcular sesgo y kurtosis
 
 
 ## Definición de funciones -----------------------------------------------------
@@ -37,29 +37,6 @@ blankPlot <- ggplot() + geom_blank(aes(1, 1)) +
     axis.ticks = element_blank(),
     axis.line = element_blank()
   )
-
-sim_st <- function(tpo, S0, mu = 0.15, sigma = 0.25, r = mu, dt = 0.01) {
-  # Simulación precio St de un activo
-  # po: tiempo en años
-  # S0: precio inicial del activo
-  # mu: retorno esperado del activo anualizado
-  # sigma: volatilidad de los retornos del activo anualizado
-  # r: tasa libre de riesgo anualizada
-  # dt: time step (debe ser menor o igual a tpo)
-  
-  steps <- tpo / dt
-  St <- vector(mode = 'numeric', length = steps)
-  St[1] <- S0
-  
-  for (i in 2:steps) {
-    
-    # $S_i=S_{i-1} e^{(r-\frac{1}{2}\sigma^2)dt + \sigma\sqrt{dt}\phi_i}$
-    St[i] <-
-      St[i - 1] * exp((r - 0.5 * sigma ^ 2)*dt + sigma * sqrt(dt) * rnorm(1))
-  }
-  
-  return(St)
-}
 
 ret_crit <- function(x, v.crit) {
   # función para identificar los valores críticos
@@ -208,9 +185,176 @@ precio_k2 <- GBSOption(TypeFlag = 'c',
                        b = tasa_lr,
                        sigma = sigma)@price
 
-(bull_spred <- precio_k1 - precio_k2)
-bullspreadcalls()
+(bull_spred <- (1 / (K2 - K1)) * (precio_k1 - precio_k2))
+
+# b)
+
+delta_k1 <- GBSGreeks(Selection = 'delta',
+                      TypeFlag = 'c',
+                      S = S0,
+                      X = K1,
+                      Time = tiempo_venc,
+                      r = tasa_lr,
+                      b = tasa_lr,
+                      sigma = sigma) # $\Delta_{K1}=0.9174$
+
+delta_k2 <- GBSGreeks(Selection = 'delta',
+                      TypeFlag = 'c',
+                      S = S0,
+                      X = K2,
+                      Time = tiempo_venc,
+                      r = tasa_lr,
+                      b = tasa_lr,
+                      sigma = sigma) # $\Delta_{K2}=0.7013$
+
+# $\sigma_{\Delta P} = \sigma_S \sqrt{\delta t} \times S_0 \times \delta$
+sigma_p <- sigma * S0 * (1 / sqrt(2))  * (delta_k2 + delta_k2) # en 6 meses
+
+nro_opciones <- 1000
+cuantil_99 <- qnorm(0.01) # -2.3263
+
+# VaR 99 % del portafolio bull spread para 1000 opciones
+VaR_p <- - nro_opciones * cuantil_99 * sigma_p
+
+# c)
+
+gamma_k1 <- GBSGreeks(Selection = 'gamma',
+                      TypeFlag = 'c',
+                      S = S0,
+                      X = K1,
+                      Time = tiempo_venc,
+                      r = tasa_lr,
+                      b = tasa_lr,
+                      sigma = sigma) # $\gamma_{K1} = 0.0224$
+
+gamma_k2 <- GBSGreeks(Selection = 'gamma',
+                      TypeFlag = 'c',
+                      S = S0,
+                      X = K2,
+                      Time = tiempo_venc,
+                      r = tasa_lr,
+                      b = tasa_lr,
+                      sigma = sigma) # $\gamma_{K2} = 0.0511$
+
+sim_mod_lin <- function(S, delta, mu = 0, sd = 1) {
+  # simula una realización del modelo lineal para $\Delta P$:
+  # $\Delta P = S \delta \Delta x$
+  
+  rnd <- rnorm(1, mu, sd)
+  delta_p <- S * delta * rnd
+  
+  return(delta_p)
+}
+
+sim_mod_cuad <- function(S, delta, gamma, mu = 0, sd = 1) {
+  # simula una realización del modelo cuadrático para $\Delta P$:
+  # $\Delta P = S \delta \Delta x + \frac{1}{2} S^2 \gamma (\Delta x)^2$
+  
+  rnd <- rnorm(1, mu, sd)
+  delta_p <- S * delta * rnd + 0.5 * S^2 * gamma * rnd^2
+  
+  return(delta_p)
+}
+
+# sim_mod_cuad2 <- function(n, S, delta, gamma, mu = 0, sd =1, r, K, tpo) {
+#   # simulación montecarlo del modelo cuadrático para $\Delta P$:
+#   # $\Delta P = S \delta \Delta x + \frac{1}{2} S^2 \gamma (\Delta x)^2$
+#   # n: número de réplicas
+#   
+#   delta_p <- vector(length = n)
+#   
+#   S_tmp <- S
+#   delta_tmp <- delta
+#   gamma_tmp <- gamma
+#   
+#   delta_p[1] <- sim_mod_cuad(S_tmp, sum(delta_tmp), sum(gamma_tmp), mu, sd)
+#   
+#   for (i in 2:n) {
+#     S_tmp <- S_tmp * rnorm(1, mu, sd)
+#     
+#     delta_tmp <- GBSGreeks(Selection = 'delta',
+#                            TypeFlag = 'c',
+#                            S = S_tmp,
+#                            X = K[1],
+#                            Time = tpo,
+#                            r = r,
+#                            b = r,
+#                            sigma = sd)[1] +
+#       GBSGreeks(Selection = 'delta',
+#                 TypeFlag = 'c',
+#                 S = S_tmp,
+#                 X = K[2],
+#                 Time = tpo,
+#                 r = r,
+#                 b = r,
+#                 sigma = sd)[1]
+#     gamma_tmp <- GBSGreeks(Selection = 'gamma',
+#                            TypeFlag = 'c',
+#                            S = S_tmp,
+#                            X = K[1],
+#                            Time = tpo,
+#                            r = r,
+#                            b = r,
+#                            sigma = sd)[1] +
+#       GBSGreeks(Selection = 'gamma',
+#                 TypeFlag = 'c',
+#                 S = S_tmp,
+#                 X = K[2],
+#                 Time = tpo,
+#                 r = r,
+#                 b = r,
+#                 sigma = sd)[1]
+#     
+#     delta_p[i] <- sim_mod_cuad(S_tmp, delta_tmp, gamma_tmp, mu, sd)
+#     
+#   }
+#   
+#   return(delta_p)
+# }
+# 
+# sim_mod_cuad2(10, S0, c(delta_k1, delta_k2), c(gamma_k1, gamma_k2),
+#               mu = 0, sd = sigma * sqrt(0.5), tasa_lr, c(K1, K2), tiempo_venc)
 
 # Para hacer el código reproducible fijemos la semilla del generador de
 # números aleatorios.
 set.seed(123)
+
+# simulación de los dos dos modelos con 10000 réplicas
+sim_c <- replicate(10000,
+                   sim_mod_cuad(S0, delta_k1 + delta_k2, gamma_k1 + gamma_k2,
+                                mu = 0, sd = sigma * sqrt(0.5)))
+sim_l <- replicate(10000,
+                   sim_mod_lin(S0, delta_k1 + delta_k2,
+                               mu = 0, sd = sigma * sqrt(0.5)))
+
+sim <- data.table(sim_c, sim_l)
+
+# media, desviación, kurtosis y sesgo de $\Delta P$
+momentos <- sim[, .(med = mean(sim_c),
+                    sd = sd(sim_c),
+                    kurt = kurtosis(sim_c) - 3,
+                    skw = skewness(sim_c))]
+
+k <- momentos[, kurt]
+s <- momentos[, skw]
+
+cuantil_cf_99 <- cuantil_99 +
+  (cuantil_99^2 - 1)*s/6 -
+  (2*cuantil_99^3 - 5*cuantil_99)*(s^2)/36 +
+  (cuantil_99^3 - 3*cuantil_99)*k/24 # teórico
+
+cuantil_m_99 <- quantile(sim[, sim_c], 0.01) # muestral
+
+VaR_cf <- - nro_opciones * cuantil_cf_99 * momentos[, sd]
+
+pdf('../img/p3c_densidades.pdf', width = 5, height = 5)
+# Comparación de las densidades
+ggplot(data = sim) + theme_bw(12) +
+  geom_density(aes(x = sim_c), colour = 'blue') +
+  geom_density(aes(x = sim_l), colour = 'black') +
+  geom_vline(xintercept = -VaR_p/1000, colour = 'black') +
+  geom_vline(xintercept = -VaR_cf/1000, colour = 'blue') +
+  xlim(sim[, min(sim)] - 20, 80) +
+  labs(x = expression(paste(Delta, ' P')), y = 'densidad')
+dev.off()
+
